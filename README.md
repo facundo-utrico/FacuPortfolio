6fb9ffca0ba4a3a8520b5e671a16cfcb5dc99547
Facundo Utrico <facundoutricowork@gmail.com>
2022-03-27 18:47:20 -0300

    Project

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
-# FacuPortfolio
-React.JS porfolio 
+﻿## 🛠️ Packages & APIs
+
+- [React](https://reactjs.org/)
+- [graphql](https://graphql.org/) 
+- [apollo-boost](https://www.apollographql.com/docs/react/get-started/)
+- [particles.js](https://github.com/VincentGarreau/particles.js/)
+- [react-reveal](https://github.com/rnosov/react-reveal)
+- [react-is-visible](https://github.com/lessp/react-is-visible)
+
+
---
+++ src/components/about/About.js
@@ -0,0 +1,99 @@
+import React from 'react'
+import './About.css'
+import Typewriter from 'typewriter-effect'
+import { Link } from 'react-scroll'
+import { Fade } from 'react-reveal'
+import Section from '../section/Section'
+import Skills from '../skills/Skills'
+
+const About = () => {
+  return (
+    <Section title="About">
+      <div className="about-content">
+        <Fade duration={1000}>
+          <div className="about-text">
+            <h2>Who am I?</h2>
+            <p>
+              I'm Facu{' '}
+              <span role="img" aria-label="lightning">
+                😄
+              </span>{' '}
+            </p>
+            <p>
+              <span role="img" aria-label="lightning">
+                ⚡
+              </span>{' '}
+              I'm a self-taught programming student who also works as a freelance developer on Fiverr.
+            </p>
+            <p>
+              <span role="img" aria-label="lightning">
+                ⚡
+              </span>{' '}
+              You can find me working with JavaScript, React, Node.js, MongoDB, Lua,
+              PHP and MYSQL.
+            </p>
+            <div className="typewriter">
+              <p className="typewriter-start">
+                <span role="img" aria-label="lightning">
+                  ⚡
+                </span>{' '}
+                I love
+              </p>
+              <Typewriter
+                options={{
+                  strings: [
+                    'learning new technologies',
+                    'play video games',
+                    'solving complex problems',
+                    'teamwork',
+                    'fixing hard-to-fix bugs',
+                    'trying new things',
+                    'collaborating with others',
+                  ],
+                  autoStart: true,
+                  loop: true,
+                }}
+              />
+              <p>.</p>
+            </div>
+            <p>
+              Since 2018 - I've spent my time seeking and learning new
+              technologies. It all started with my passion for video games, I learned to program in LUA to create a server in FiveM and then I met the beautiful world of IT and started my career as a freelancer in{' '}
+              <Link
+                className="textLink"
+                to="Fiverr"
+                spy={true}
+                smooth={true}
+                duration={500}
+              >
+                Fiverr
+              </Link>
+              , I worked with {' '}
+                amazing people and made a lot of friends!
+            </p>
+            <div className="location-wrapper">
+              <svg
+                className="octicon octicon-location"
+                viewBox="0 0 16 16"
+                version="1.1"
+                width="16"
+                height="16"
+                aria-hidden="true"
+              >
+                <path
+                  fill="white"
+                  fillRule="evenodd"
+                  d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"
+                ></path>
+              </svg>
+              <p>Montevideo, Uruguay</p>
+            </div>
+          </div>
+        </Fade>
+        <Skills />
+      </div>
+    </Section>
+  )
+}
+
+export default About
